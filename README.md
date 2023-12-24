61a0c8af1dee4c808d040dba905fcd760167c1d6
Vincent Senjaya <73159146+vincentsenjaya24@users.noreply.github.com>
2023-12-24 20:31:11 +0700

    Add PPT and Readme

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# BP12_FinalProjectPSD
+
+# MRT Ticketing Machine
+
+Proyek MRT Ticketing Machine bertujuan untuk merancang dan mensimulasikan sebuah mesin penjualan tiket Mass Rapid Transit (MRT) yang efisien dan user-friendly. Mesin ini akan memfasilitasi proses pembelian tiket dengan menyediakan tiga opsi tiket dengan harga berbeda (5k, 10k, dan 15k). Penumpang akan dapat memilih tiket yang diinginkan dan melakukan pembayaran menggunakan pecahan uang tertentu (5k, 10k, 20k, 50k, 100k rupiah). Selain itu,  sistem juga diharapkan dapat melakukan error handling terhadap input yang tidak sesuai.
+
+# Implementation
+
+Ticketing machine ini memiliki 3 input dan akan menghasilkan 2 output, berikut penjelasan tiap portnya:
+#### Input:
+- CLK: Input clock untuk mengatur timing dan siklus program
+- T: Input yang merepresentasikan tipe tiket yang bervariasi(5k, 10k, 15k)
+- M: Input yang merepresentasikan uang yang diinput user untuk membayar tiket(1k, 2k, 5k, 10k, 20k, 50k, 100k)
+#### Output:
+- C: Output yang merepresentasikan kembalian dari hasil pembelian tiket
+- O: Output yang merepresentasikan tiket yang telah dibeli, O hanya meneruskan input T di awal
+
+# Code Explanation
+
+- Entity Declaration: Declares an entity named Ticket_Machine with input ports CLK, T, and M, and output ports C and O. T, M, C, and O are vectors of specific sizes.
+```
+ENTITY Ticket_Machine IS
+	PORT (
+		CLK : IN STD_LOGIC;
+		T : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+		M : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+		C : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+		O : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+		-- CLK 	= clock
+		-- T	= Jenis tiket
+		-- M 	= Jenis uang
+		-- C	= Jenis uang yang akan dikeluarkan
+		-- O	= Jenis tiket yang akan dikeluarkan
+	);
+END Ticket_Machine;
+```
+
+- Clock Process: A synchronous process triggered on the rising edge of the clock (CLK). Updates the present state (PS) with the next state (NS).
+```
+	sync_proc : PROCESS (CLK)
+	BEGIN
+		IF (rising_edge(CLK)) THEN
+			PS <= NS;
+		END IF;
+	END PROCESS;
+```
+
+-  Combinational Process: A process sensitive to changes in PS, M, and T. It contains the main state transition and control logic for the ticket machine
+```
+comb_proc : PROCESS (PS, M, T)
+BEGIN
+    -- Main state transition and control logic
+END PROCESS;
+```
+- FSM implementation: There are 6 state in this project, including the idle state and the reset state.
+```
+CASE PS IS
+    WHEN S0 => -- idle
+    WHEN S1 => -- menunggu uang masuk
+    WHEN S2 => -- Menambahkan uang ke total dalam 
+    WHEN S3 => -- Mencek apakah uang dalam machine melebihi harga tiket.
+    WHEN S4 => -- Melakukan perhitungan uang kembalian.
+    WHEN S5 => -- Mencek apakah uang masih ada yang perlu dikembalikan.
+    WHEN S6 => -- Reset
+END CASE;
+```
+- Report Block: A block to report changes after the transaction occured.
+```
+FOR i IN 0 TO 3 LOOP
+	REPORT "Change=" & INTEGER'image(changes_array(i));
+END LOOP;
+```
--- Ticket_Machine.vhd
+++ Ticket_Machine.vhd
@@ -2,29 +2,48 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 -- Untuk M (Money)
 -- 000 = Tidak ada
--- 001 = 5K
--- 010 = 10K
--- 011 = 20K
--- 100 = 50K
--- 101 = 100K
+-- 001 = 1K
+-- 010 = 2K
+-- 011 = 5K
+-- 100 = 10K
+-- 101 = 20K
+-- 110 = 50K
+-- 111 = 100K
+
 -- Untuk T (Ticket)
 -- 00 = Tidak ada
 -- 01 = 5K
 -- 10 = 10K
 -- 11 = 15K
+
+--- Untuk C (Change)
+-- 000 = Tidak ada
+-- 001 = 1K
+-- 010 = 2K
+-- 011 = 5K
+-- 100 = 10K
+-- 101 = 20K
+-- 110 = 50K
+-- 111 = 100K
 ENTITY Ticket_Machine IS
 	PORT (
 		CLK : IN STD_LOGIC;
-		M : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
 		T : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-		C1, C2, C3, C4 : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-		O : OUT STD_LOGIC_VECTOR (1 DOWNTO 0)
+		M : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+		C : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+		O : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+		-- CLK 	= clock
+		-- T	= Jenis tiket
+		-- M 	= Jenis uang
+		-- C	= Jenis uang yang akan dikeluarkan
+		-- O	= Jenis tiket yang akan dikeluarkan
 	);
 END Ticket_Machine;
 
 ARCHITECTURE behaviour OF Ticket_Machine IS
-	TYPE state_types IS (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14);
-
+	TYPE state_types IS (S0, S1, S2, S3, S4, S5, S6);
+	TYPE t_changes_array IS ARRAY (0 TO 3) OF INTEGER;
+	SIGNAL changes_array : t_changes_array := (0, 0, 0, 0);
 	-- S0 = idle
 	-- S1 = pilih tiket 5K
 	-- S2 = pilih tiket 10K
@@ -33,16 +52,21 @@ ARCHITECTURE behaviour OF Ticket_Machine IS
 	-- S5 = kembali 5K
 	-- S6 = kembali 10K
 	-- S7 = kembali 15K (10K + 5K)
-	-- S8 = kembali 35K (10K + 5K)
-	-- S9 = kembali 40K (10K + 5K)
-	-- S10 = kembali 45K (10K + 5K)
-	-- S11 = kembali 85K (10K + 5K)
-	-- S12 = kembali 80K (10K + 5K)
-	-- S13 = kembali 95K (10K + 5K)
-	-- S14 = keluar tiket
-	SIGNAL PS, NS, temp_NS : state_types;
+	-- S8 = keluar tiket
+
+	SIGNAL PS, NS : state_types;
+	-- PS = Present State
+	-- NS = Next State
+	SIGNAL currentIndex : INTEGER := 0;
+	SIGNAL ticketPrice : INTEGER RANGE 0 TO 255 := 0;
+	SIGNAL inputMoney : INTEGER RANGE 0 TO 255 := 0;
+	SIGNAL currentMoney : INTEGER RANGE 0 TO 255 := 0;
+	SIGNAL changeMoney : INTEGER RANGE 0 TO 255 := 0;
+	-- ticketPrice 	= Mengubah tipe tiket menjadi integer
+	-- inputMoney 	= Mengubah tipe input uang menjadi integer
+	-- currentMoney	= Melacak dan menghitung jumlah uang yang ada didalam mesin.
 BEGIN
-	sync_proc : PROCESS (CLK, NS)
+	sync_proc : PROCESS (CLK)
 	BEGIN
 		IF (rising_edge(CLK)) THEN
 			PS <= NS;
@@ -50,175 +74,133 @@ BEGIN
 	END PROCESS;
 
 	comb_proc : PROCESS (PS, M, T)
+		-- variable
 	BEGIN
-		C1 <= "000"; -- 0
-		C2 <= "000"; -- 0
-		C3 <= "000"; -- 0
-		C4 <= "000"; -- 0
-		O <= "00";
-
 		CASE PS IS
-
 			WHEN S0 => -- idle
-				IF (T = "00" OR M = "000") THEN
+				IF (T = "00" OR M /= "UUU") THEN
 					NS <= S0;-- input invalid
-
-				ELSIF (T = "01" AND M /= "000") THEN
-					NS <= S1; -- tiket 5K
-
-				ELSIF (T = "10" AND M /= "000") THEN
-					NS <= S2;-- tiket 10K
-
-				ELSIF (T = "11" AND M /= "000") THEN
-					NS <= S3; -- tiket 15K
+				ELSE
+					CASE T IS
+						WHEN "01" =>
+							ticketPrice <= 5;
+							NS <= S1;
+						WHEN "10" =>
+							ticketPrice <= 10;
+							NS <= S1;
+						WHEN "11" =>
+							ticketPrice <= 15;
+							NS <= S1;
+						WHEN OTHERS =>
+							NS <= S0;
+					END CASE;
 				END IF;
-				
-			WHEN S1 => -- tiket 5K
+
+			WHEN S1 => -- menunggu uang masuk
 				IF (M = "000") THEN
-					NS <= S0; -- kembali ke S0
-
-				ELSIF (M = "001") THEN
-					NS <= S4; -- kembali 0K
-
-				ELSIF (M = "010") THEN
-					NS <= S5; -- kembali 5k
-
-				ELSIF (M = "011") THEN
-					NS <= S7; -- kembali 15K --> 10 + 5
-
-				ELSIF (M = "100") THEN
-					NS <= S10; -- kembali 45K --> 40 + 5
-
-				ELSIF (M = "101") THEN
-					NS <= S13; -- kembali 95K --> 50 + 20 + 20 + 5
-
+					NS <= S1;
+				ELSE
+					CASE M IS
+						WHEN "001" =>
+							inputMoney <= 1;
+							NS <= S2;
+						WHEN "010" =>
+							inputMoney <= 2;
+							NS <= S2;
+						WHEN "011" =>
+							inputMoney <= 5;
+							NS <= S2;
+						WHEN "100" =>
+							inputMoney <= 10;
+							NS <= S2;
+						WHEN "101" =>
+							inputMoney <= 20;
+							NS <= S2;
+						WHEN "110" =>
+							inputMoney <= 50;
+							NS <= S2;
+						WHEN "111" =>
+							inputMoney <= 100;
+							NS <= S2;
+						WHEN OTHERS =>
+							NS <= S1;
+					END CASE;
 				END IF;
 
-			WHEN S2 => -- tiket 10K
-				IF (M = "000") THEN
-					NS <= S0; -- kembali ke S0
+			WHEN S2 => -- Menambahkan uang ke total dalam 
+				currentMoney <= currentMoney + inputMoney;
+				NS <= S3;
 
-				ELSIF (M = "001") THEN
-					NS <= S1; -- sisa 5k
-
-				ELSIF (M = "010") THEN
-					NS <= S4; -- kembali 0
-
-				ELSIF (M = "011") THEN
-					NS <= S6; -- kembali 10k
-
-				ELSIF (M = "100") THEN
-					NS <= S9; -- kembali 40k
-
-				ELSIF (M = "101") THEN
-					NS <= S12; -- kembali 90k
-
+			WHEN S3 => -- Mencek apakah uang dalam machine melebihi harga tiket.
+				IF currentMoney >= ticketPrice THEN
+					O <= T;
+					changeMoney <= currentMoney - ticketPrice;
+					NS <= S4;
+				ELSE
+					NS <= S1;
 				END IF;
 
-			WHEN S3 => -- tiket 15K
+			WHEN S4 => -- Melakukan perhitungan uang kembalian.
+				IF changeMoney >= 100 THEN
+					changeMoney <= changeMoney - 100;
+					changes_array(currentIndex) <= 100;
+					C <= "111";
+				ELSIF changeMoney >= 50 THEN
+					changeMoney <= changeMoney - 50;
+					changes_array(currentIndex) <= 50;
 
-				IF (M = "000") THEN
-					NS <= S0; -- kembali ke S0
+					C <= "110";
+				ELSIF changeMoney >= 20 THEN
+					changeMoney <= changeMoney - 20;
+					changes_array(currentIndex) <= 20;
 
-				ELSIF (M = "001") THEN
-					NS <= S2; -- sisa 10k
+					C <= "101";
+				ELSIF changeMoney >= 10 THEN
+					changeMoney <= changeMoney - 10;
+					changes_array(currentIndex) <= 10;
 
-				ELSIF (M = "010") THEN
-					NS <= S1; -- sisa 5k
+					C <= "100";
+				ELSIF changeMoney >= 5 THEN
+					changeMoney <= changeMoney - 5;
+					changes_array(currentIndex) <= 5;
 
-				ELSIF (M = "011") THEN
-					NS <= S5; -- kembali 5k
+					C <= "011";
+				ELSIF changeMoney >= 2 THEN
+					changeMoney <= changeMoney - 2;
+					changes_array(currentIndex) <= 2;
 
-				ELSIF (M = "100") THEN
-					NS <= S8; -- kembali 35k
+					C <= "010";
+				ELSIF changeMoney >= 1 THEN
+					changeMoney <= changeMoney - 1;
+					changes_array(currentIndex) <= 1;
 
-				ELSIF (M = "101") THEN
-					NS <= S11; -- kembali 85k
+					C <= "001";
+				ELSE
+					C <= "000";
+					changes_array(currentIndex) <= 0;
 
 				END IF;
-				--- Untuk C1 dan C2 (Change)
-				-- 000 = 0
-				-- 001 = 5K
-				-- 010 = 10K
-				-- 011 = 20K
-				-- 100 = 50K
-			WHEN S4 => -- kembali 0
-				C1 <= "000"; -- 0
-				C2 <= "000"; -- 0
-				C3 <= "000"; -- 0
-				C4 <= "000"; -- 0
-				NS <= S14;
+				currentIndex <= currentIndex + 1;
+				NS <= S5;
 
-			WHEN S5 => -- kembali 5K (5 + 0 + 0 + 0)
-				C1 <= "001"; -- 5
-				C2 <= "000"; -- 0
-				C3 <= "000"; -- 0
-				C4 <= "000"; -- 0
-				NS <= S14;
+			WHEN S5 => -- Mencek apakah uang masih ada yang perlu dikembalikan.
+				IF changeMoney > 0 THEN
+					NS <= S4;
+				ELSE
+					NS <= S6;
+				END IF;
 
-			WHEN S6 => -- kembali 10K (10 + 0 + 0 + 0)
-				C1 <= "010"; -- 10
-				C2 <= "000"; -- 0
-				C3 <= "000"; -- 0
-				C4 <= "000"; -- 0
-				NS <= S14;
-
-			WHEN S7 => -- kembali 15K (10 + 5K + 0 + 0)
-				C1 <= "010"; -- 10
-				C2 <= "001"; -- 5
-				C3 <= "000"; -- 0
-				C4 <= "000"; -- 0
-				NS <= S14;
-
-			WHEN S8 => -- kembali 35K (20 + 10 + 5K + 0)
-				C1 <= "011"; -- 20
-				C2 <= "010"; -- 10
-				C3 <= "001"; -- 5
-				C4 <= "000"; -- 0
-				NS <= S14;
-
-			WHEN S9 => -- kembali 40K (20 + 20 + 0 + 0)
-				C1 <= "011"; -- 20
-				C2 <= "011"; -- 20
-				C3 <= "000"; -- 0
-				C4 <= "000"; -- 0
-				NS <= S14;
-
-			WHEN S10 => -- kembali 45K (20 + 20 + 5 + 0)
-				C1 <= "011"; -- 20
-				C2 <= "011"; -- 20
-				C3 <= "001"; -- 5
-				C4 <= "000"; -- 0
-				NS <= S14;
-
-			WHEN S11 => -- kembali 85K (50 + 20 + 10 + 5)
-				C1 <= "100"; -- 50
-				C2 <= "011"; -- 20
-				C3 <= "010"; -- 10
-				C4 <= "001"; -- 5
-				NS <= S14;
-
-			WHEN S12 => -- kembali 90K (50 + 20 + 20 + 0)
-				C1 <= "100"; -- 50
-				C2 <= "011"; -- 20
-				C3 <= "011"; -- 20
-				C4 <= "000"; -- 0
-				NS <= S14;
-
-			WHEN S13 => -- kembali 95K (50 + 20 + 20 + 5)
-				C1 <= "100"; -- 50
-				C2 <= "011"; -- 20
-				C3 <= "011"; -- 20
-				C4 <= "001"; -- 5
-				NS <= S14;
-
-			WHEN S14 => -- keluar tiket
-				O <= T;
-
+			WHEN S6 => -- Reset
+				ticketPrice <= 0;
+				inputMoney <= 0;
+				currentMoney <= 0;
+				changeMoney <= 0;
 				NS <= S0;
-
 		END CASE;
 
+		FOR i IN 0 TO 3 LOOP
+			REPORT "Change=" & INTEGER'image(changes_array(i));
+		END LOOP;
 	END PROCESS;
 
 END behaviour;
